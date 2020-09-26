f09128984470d99eb120002924f5abdc5d3a115c
Michael Szymczak <contact@michaelszymczak.com>
2020-09-26 19:13:33 +0100

    #1 Start turning data into messages

--- asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/MessageEncodingApplication.java
+++ asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/MessageEncodingApplication.java
@@ -1,16 +1,28 @@
 package dev.squaremile.asynctcpacceptance.sampleapps;
 
+import java.nio.ByteBuffer;
+
+
 import dev.squaremile.asynctcp.api.app.Application;
 import dev.squaremile.asynctcp.api.app.Event;
+import dev.squaremile.asynctcp.api.events.DataReceived;
+import dev.squaremile.asynctcp.api.events.MessageReceived;
+import dev.squaremile.asynctcp.api.values.ConnectionIdValue;
+import dev.squaremile.asynctcp.api.values.PredefinedTransportEncoding;
 
 public class MessageEncodingApplication implements Application
 {
     private final Application delegate;
+    private final PredefinedTransportEncoding encoding;
 
-    public MessageEncodingApplication(final Application delegate)
+    public MessageEncodingApplication(final Application delegate, final PredefinedTransportEncoding encoding)
     {
-
+        if (encoding != PredefinedTransportEncoding.SINGLE_BYTE)
+        {
+            throw new IllegalArgumentException(encoding + " is not supported yet");
+        }
         this.delegate = delegate;
+        this.encoding = encoding;
     }
 
     @Override
@@ -34,6 +46,14 @@ public class MessageEncodingApplication implements Application
     @Override
     public void onEvent(final Event event)
     {
-        delegate.onEvent(event);
+        if (event instanceof DataReceived)
+        {
+            // TODO: implement actual logic
+            delegate.onEvent(new MessageReceived(new ConnectionIdValue(8808, 100)).set(ByteBuffer.wrap(new byte[]{5}), 1));
+        }
+        else
+        {
+            delegate.onEvent(event);
+        }
     }
 }
--- asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/MessageEncodingApplicationTest.java
+++ asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/MessageEncodingApplicationTest.java
@@ -1,5 +1,6 @@
 package dev.squaremile.asynctcpacceptance.sampleapps;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,20 +11,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.squaremile.asynctcp.api.app.Application;
 import dev.squaremile.asynctcp.api.app.Event;
+import dev.squaremile.asynctcp.api.events.DataReceived;
+import dev.squaremile.asynctcp.api.events.MessageReceived;
 import dev.squaremile.asynctcp.api.events.StartedListening;
+import dev.squaremile.asynctcp.api.values.ConnectionIdValue;
 
+import static dev.squaremile.asynctcp.api.values.PredefinedTransportEncoding.SINGLE_BYTE;
+import static java.nio.ByteBuffer.wrap;
 import static java.util.Arrays.asList;
 
 class MessageEncodingApplicationTest
 {
 
-    private ApplicationSpy applicationSpy = new ApplicationSpy();
+    private final ApplicationSpy spy = new ApplicationSpy();
+    private final MessageEncodingApplication app = new MessageEncodingApplication(spy, SINGLE_BYTE);
 
     @Test
     void shouldDelegateToUnderlyingApplication()
     {
-        MessageEncodingApplication app = new MessageEncodingApplication(applicationSpy);
-        assertThat(applicationSpy.invoked()).isEmpty();
+        // Given
+        assertThat(spy.invoked()).isEmpty();
 
         // When
         app.onStart();
@@ -32,16 +39,48 @@ class MessageEncodingApplicationTest
         app.onEvent(new StartedListening(8888, 5));
 
         // Then
-        assertThat(applicationSpy.invoked()).isEqualTo(asList(
-                "onStart", "onStop", "work", "onEvent: " + new StartedListening(8888, 5).toString()
-        ));
+        assertEquals(
+                spy.invoked(),
+                "onStart",
+                "onStop",
+                "work",
+                new StartedListening(8888, 5)
+        );
+    }
+
+    private void assertEquals(final List<Object> actual, final Object... expected)
+    {
+        assertThat(actual).usingRecursiveComparison().isEqualTo(asList(expected));
+    }
+
+    @Test
+    void shouldPassMessagesThrough()
+    {
+        // When
+        MessageReceived messageReceived1 = new MessageReceived(new ConnectionIdValue(8899, 4)).set(ByteBuffer.wrap(new byte[]{1, 2, 3, 4, 5, 6, 7}), 5);
+        MessageReceived messageReceived2 = new MessageReceived(new ConnectionIdValue(6234, 5)).set(ByteBuffer.wrap(new byte[]{0}), 1);
+        app.onEvent(messageReceived1);
+        app.onEvent(messageReceived2);
+
+        // Then
+        assertEquals(spy.invoked(), messageReceived1, messageReceived2);
+    }
+
+    @Test
+    void shouldEncodeDataAsMessage()
+    {
+        // When
+        app.onEvent(new DataReceived(8808, 100, 1, 1, 30, wrap(new byte[]{5})));
+
+        // Then
+        assertEquals(spy.invoked(), new MessageReceived(new ConnectionIdValue(8808, 100)).set(ByteBuffer.wrap(new byte[]{5}), 1));
     }
 
     private static class ApplicationSpy implements Application
     {
-        private final List<String> invoked = new ArrayList<>();
+        private final List<Object> invoked = new ArrayList<>();
 
-        List<String> invoked()
+        List<Object> invoked()
         {
             return invoked;
         }
@@ -67,7 +106,7 @@ class MessageEncodingApplicationTest
         @Override
         public void onEvent(final Event event)
         {
-            invoked.add("onEvent: " + event.toString());
+            invoked.add(event);
         }
     }
 }
