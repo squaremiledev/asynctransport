7b2bd55185ffc1a020e0a2bfce22c0f52e504c17
Michael Szymczak <contact@michaelszymczak.com>
2020-10-07 23:25:48 +0100

    #8 Finish writing a FIX delineation/reassembly logic
    Passes all the tests, but the logic requires some cleanup as not really elegant

--- serialization/src/main/java/dev/squaremile/asynctcp/serialization/internal/delineation/FixMessageDelineation.java
+++ serialization/src/main/java/dev/squaremile/asynctcp/serialization/internal/delineation/FixMessageDelineation.java
@@ -1,10 +1,21 @@
 package dev.squaremile.asynctcp.serialization.internal.delineation;
 
 import org.agrona.DirectBuffer;
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
 
 public class FixMessageDelineation implements DelineationHandler
 {
+    private static final char SOH = '\u0001';
+    private static final int CHECKSUM_SIZE = 7;
     private final DelineationHandler delineatedDataHandler;
+    private LengthScanState lengthScanState = LengthScanState.START;
+    private State state = State.LENGTH_UNKNOWN;
+    private int declaredLength;
+    private int bytesBeforeDeclaredLength;
+    private int unprocessedLength;
+    // TODO #8: decide based on some evidence, such as max inbound buffer size
+    private MutableDirectBuffer unprocessed = new UnsafeBuffer(new byte[1024]);
 
     public FixMessageDelineation(final DelineationHandler delineatedDataHandler)
     {
@@ -14,9 +25,109 @@ public class FixMessageDelineation implements DelineationHandler
     @Override
     public void onData(final DirectBuffer buffer, final int offset, final int length)
     {
-        if (length > 0)
+        int bytesProcessedInThisRun = 0;
+        do
         {
-            delineatedDataHandler.onData(buffer, offset, length);
+            if (state == State.LENGTH_UNKNOWN)
+            {
+                calculateSize(buffer, offset + bytesProcessedInThisRun, length);
+                if (state != State.LENGTH_KNOWN)
+                {
+                    unprocessed.putBytes(unprocessedLength, buffer, offset + bytesProcessedInThisRun, length);
+                    unprocessedLength += length;
+                    return;
+                }
+            }
+            if (state == State.LENGTH_KNOWN)
+            {
+                int messageLength = bytesBeforeDeclaredLength + declaredLength + CHECKSUM_SIZE;
+                if (bytesProcessedInThisRun + messageLength > length + unprocessedLength)
+                {
+                    // We are (or still are) not able to reach the end of the message
+                    int remainingButInsufficient = length - bytesProcessedInThisRun;
+                    buffer.getBytes(offset + bytesProcessedInThisRun, unprocessed, unprocessedLength, remainingButInsufficient);
+                    unprocessedLength += remainingButInsufficient;
+                    return;
+                }
+                else if (unprocessedLength > 0)
+                {
+                    // We have enough data when combining unprocessed before and current one
+                    int remainingLengthToCompleteMessage = messageLength - unprocessedLength;
+                    unprocessed.putBytes(unprocessedLength, buffer, offset + bytesProcessedInThisRun, remainingLengthToCompleteMessage);
+                    delineatedDataHandler.onData(unprocessed, 0, messageLength);
+                    declaredLength = 0;
+                    bytesBeforeDeclaredLength = 0;
+                    lengthScanState = LengthScanState.START;
+                    state = State.LENGTH_UNKNOWN;
+                    unprocessedLength = 0;
+                    bytesProcessedInThisRun += remainingLengthToCompleteMessage;
+                }
+                else
+                {
+                    // We have enough data in the current buffer
+                    delineatedDataHandler.onData(buffer, offset + bytesProcessedInThisRun, messageLength);
+                    declaredLength = 0;
+                    bytesBeforeDeclaredLength = 0;
+                    lengthScanState = LengthScanState.START;
+                    state = State.LENGTH_UNKNOWN;
+                    unprocessedLength = 0;
+                    bytesProcessedInThisRun += messageLength;
+                }
+            }
         }
+        while (bytesProcessedInThisRun < length);
+    }
+
+    private void calculateSize(final DirectBuffer buffer, final int offset, final int length)
+    {
+        for (int c = 0; c < length; c++)
+        {
+            bytesBeforeDeclaredLength++;
+            char ch = (char)buffer.getByte(offset + c);
+            if (lengthScanState == LengthScanState.SIZE_VALUE)
+            {
+                if (ch >= '0' && ch <= '9')
+                {
+                    declaredLength = declaredLength * 10 + ch - '0';
+                }
+                else
+                {
+                    lengthScanState = LengthScanState.START;
+                    state = State.LENGTH_KNOWN;
+                    return;
+                }
+            }
+            else if (ch == SOH)
+            {
+                lengthScanState = LengthScanState.SPECIAL;
+            }
+            else if (lengthScanState == LengthScanState.SPECIAL && ch == '9')
+            {
+                lengthScanState = LengthScanState.SIZE;
+            }
+            else if (lengthScanState == LengthScanState.SIZE && ch == '=')
+            {
+                lengthScanState = LengthScanState.SIZE_VALUE;
+            }
+            else
+            {
+                lengthScanState = LengthScanState.NORMAL;
+            }
+        }
+    }
+
+    private enum State
+    {
+        LENGTH_UNKNOWN,
+        LENGTH_KNOWN
+    }
+
+    private enum LengthScanState
+    {
+        START,
+        NORMAL,
+        SIZE,
+        SIZE_VALUE,
+        SPECIAL
     }
 }
--- serialization/src/test/java/dev/squaremile/asynctcp/serialization/internal/delineation/FixMessageDelineationTest.java
+++ serialization/src/test/java/dev/squaremile/asynctcp/serialization/internal/delineation/FixMessageDelineationTest.java
@@ -1,6 +1,8 @@
 package dev.squaremile.asynctcp.serialization.internal.delineation;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +17,14 @@ class FixMessageDelineationTest
     private final DelineatedDataSpy delineatedDataSpy = new DelineatedDataSpy();
     private final FixMessageDelineation delineation = new FixMessageDelineation(delineatedDataSpy);
 
-    private static byte[] ascii(final String content)
+    private static byte[] asciiFix(final String content)
     {
-        return content.replaceAll("^", "\u0001").getBytes(StandardCharsets.US_ASCII);
+        return content.replaceAll("\\^", "\u0001").getBytes(StandardCharsets.US_ASCII);
+    }
+
+    private static String str(final byte[] content)
+    {
+        return new String(content, StandardCharsets.US_ASCII);
     }
 
     @Test
@@ -26,15 +33,109 @@ class FixMessageDelineationTest
         delineation.onData(bufferWith(new byte[]{}), 0, 0);
         delineation.onData(bufferWith(new byte[]{1}), 1, 0);
 
-        assertThat(delineatedDataSpy.received()).isEmpty();
+        assertThat(notified()).isEmpty();
     }
 
     @Test
     void shouldDetectFixMessage()
     {
-        byte[] data = ascii("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^");
-        delineation.onData(bufferWith(bytes(new byte[3], data, new byte[2])), 3, data.length);
+        delineation.onData(bufferWith(bytes(new byte[3], completeFixMessage(), new byte[2])), 3, completeFixMessage().length);
 
-        assertThat(delineatedDataSpy.received()).containsExactly(data);
+        assertThat(notified()).containsExactly(str(completeFixMessage()));
+    }
+
+    @Test
+    void shouldDetectDelineationPointBetweenMessages()
+    {
+        byte[] content1 = asciiFix("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^");
+        byte[] content2 = asciiFix("8=FIX.4.0^9=55^35=0^49=BuyS^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^");
+
+        delineation.onData(bufferWith(bytes(content1, content2)), 0, content1.length + content2.length);
+
+        assertThat(notified()).containsExactly(str(content1), str(content2));
+    }
+
+    @Test
+    void shouldDelineateAndReassembleMultipleMessages()
+    {
+        byte[] content1 = asciiFix("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^8=FIX.4.0^9=55^35=0^49=BuyS^5");
+        byte[] content2 = asciiFix("6=SellSide^34=5^52=20190605-11:57:29.363^10=175^8=FIX.4.0^9");
+        byte[] content3 = asciiFix("=50^35=0^49=BuyS^56=Sel^34=5^52=20190605-11:57:29.363^10=175^");
+
+        delineation.onData(bufferWith(bytes(content1, content2, content3)), 0, content1.length + content2.length + content3.length);
+
+        assertThat(notified()).containsExactly(
+                str(asciiFix("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^")),
+                str(asciiFix("8=FIX.4.0^9=55^35=0^49=BuyS^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^")),
+                str(asciiFix("8=FIX.4.0^9=50^35=0^49=BuyS^56=Sel^34=5^52=20190605-11:57:29.363^10=175^"))
+        );
+    }
+
+    @Test
+    void shouldNotNotifyAboutIncompleteMessage()
+    {
+        delineation.onData(bufferWith(completeFixMessage()), 0, completeFixMessage().length - 1);
+
+        assertThat(notified()).isEmpty();
+    }
+
+    @Test
+    void shouldHandleMessagesArrivingInTwoChunks()
+    {
+        delineation.onData(bufferWith(completeFixMessage()), 0, completeFixMessage().length - 1);
+        assertThat(notified()).isEmpty();
+
+        delineation.onData(bufferWith(completeFixMessage()), completeFixMessage().length - 1, 1);
+        assertThat(notified()).containsExactly(str(completeFixMessage()));
+    }
+
+    @Test
+    void shouldHandleMessagesArrivingInMultipleChunks()
+    {
+        byte[] partA = asciiFix(" 8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.3  ");
+        byte[] partB = asciiFix("  63^1 ");
+        byte[] partC = asciiFix("   0=175^ ");
+        delineation.onData(bufferWith(partA), 1, partA.length - 3);
+        assertThat(notified()).isEmpty();
+
+        delineation.onData(bufferWith(partB), 2, 4);
+        assertThat(notified()).isEmpty();
+
+        delineation.onData(bufferWith(partC), 3, 6);
+        assertThat(notified()).containsExactly(str(asciiFix("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^")));
+    }
+
+    @Test
+    void shouldReAssembleMessagesFromIndividualBytes()
+    {
+        byte[] fullMessage = completeFixMessage();
+
+        for (int i = 0; i < fullMessage.length; i++)
+        {
+            delineation.onData(bufferWith(fullMessage), i, 1);
+        }
+
+        assertThat(notified()).containsExactly(str(fullMessage));
+    }
+
+    @Test
+    void shouldOnlyConsiderLengthFieldWhenCalculatingLength()
+    {
+        // technically not correct fix, as the length must be the second field, but useful for state machine validation
+        byte[] message = asciiFix("8=FIX.4.0^99=28^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^");
+
+        delineation.onData(bufferWith(message), 0, message.length);
+
+        assertThat(notified()).containsExactly(str(asciiFix("8=FIX.4.0^99=28^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^")));
+    }
+
+    private List<String> notified()
+    {
+        return delineatedDataSpy.received().stream().map(FixMessageDelineationTest::str).collect(Collectors.toList());
+    }
+
+    private byte[] completeFixMessage()
+    {
+        return asciiFix("8=FIX.4.0^9=58^35=0^49=BuySide^56=SellSide^34=5^52=20190605-11:57:29.363^10=175^");
     }
 }
