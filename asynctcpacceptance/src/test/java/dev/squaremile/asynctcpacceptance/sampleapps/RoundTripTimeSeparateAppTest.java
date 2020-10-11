073ee8698c72e68966892a441cc0cff67ed75d2d
Michael Szymczak <contact@michaelszymczak.com>
2020-10-11 18:41:14 +0100

    #6 Measure round trip times between separate JVMs

--- asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/RoundTripTimeSeparateAppTest.java
+++ asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/sampleapps/RoundTripTimeSeparateAppTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.HdrHistogram.Histogram;
 import org.agrona.collections.MutableBoolean;
 import org.agrona.collections.MutableLong;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
@@ -17,24 +19,27 @@ import static dev.squaremile.asynctcp.transport.testfixtures.FreePort.freePort;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
+@Disabled
 public class RoundTripTimeSeparateAppTest
 {
-    private static final int WARM_UP = 10_000;
-    private static final int TIMES_MEASURED = 100_000;
+    private static final int WARM_UP = 100_000;
+    private static final int TIMES_MEASURED = 1_000_000;
     private static final int TOTAL = WARM_UP + TIMES_MEASURED;
     private static final Histogram HISTOGRAM = new Histogram(TimeUnit.SECONDS.toNanos(10), 3);
     private final MutableBoolean isDone = new MutableBoolean(false);
     private final MutableLong startedNanos = new MutableLong(-1);
     private final MutableLong stoppedNanos = new MutableLong(-1);
     private final MutableLong completeRoundTrips = new MutableLong(0);
+    private Application echo;
 
     @Test
     void measureRoundTripTime()
     {
-        int port = freePort();
+        int port = freePort(8889);
         MutableBoolean isReady = new MutableBoolean(false);
         Application source = sourceApplication(port, isReady);
-        Application echo = echoApplication(port);
+//        echo = echoApplication(port);
+        echo = noOpApplication(port);
 
         source.onStart();
         while (!isReady.get())
@@ -62,6 +67,38 @@ public class RoundTripTimeSeparateAppTest
         System.out.print(" which took " + MILLISECONDS.toSeconds(tookMs) + " seconds");
         System.out.println();
         System.out.println("99.99th percentile is " + HISTOGRAM.getValueAtPercentile(99.99) + " microseconds");
+    }
+
+    @Test
+    void runSeparateJvmEchoApplication()
+    {
+        echo = echoApplication(8889);
+        echo.onStart();
+        while (true)
+        {
+            echo.work();
+        }
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        if (echo != null)
+        {
+            echo.onStop();
+            for (int i = 0; i < 100; i++)
+            {
+                echo.work();
+            }
+        }
+    }
+
+    private Application noOpApplication(final int port)
+    {
+        return event ->
+        {
+
+        };
     }
 
     private Application echoApplication(final int port)
