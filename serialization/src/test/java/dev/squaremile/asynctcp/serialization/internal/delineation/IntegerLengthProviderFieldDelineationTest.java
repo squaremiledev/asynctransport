c4173ad8b932150b26a93b2da73a4c8e1e407be2
Michael Szymczak <contact@michaelszymczak.com>
2020-10-25 20:46:55 +0000

    #8 Write failing test case for padding with multiple batches length

--- serialization/src/test/java/dev/squaremile/asynctcp/serialization/internal/delineation/IntegerLengthProviderFieldDelineationTest.java
+++ serialization/src/test/java/dev/squaremile/asynctcp/serialization/internal/delineation/IntegerLengthProviderFieldDelineationTest.java
@@ -2,6 +2,7 @@ package dev.squaremile.asynctcp.serialization.internal.delineation;
 
 import java.nio.ByteBuffer;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -126,8 +127,8 @@ class IntegerLengthProviderFieldDelineationTest
     @Test
     void shouldUseLengthBytesFromThePreviousBatch()
     {
-        final IntegerLengthFieldDelineation delineation = new IntegerLengthFieldDelineation(delineatedDataSpy, 0);
-        delineation.onData(bufferWith(b(intInBytes(3)[0], intInBytes(3)[1], intInBytes(3)[2], intInBytes(3)[3])), 0, 4);
+        final IntegerLengthFieldDelineation delineation = new IntegerLengthFieldDelineation(delineatedDataSpy, 2);
+        delineation.onData(bufferWith(b(PADDING, PADDING, intInBytes(3)[0], intInBytes(3)[1], intInBytes(3)[2], intInBytes(3)[3])), 0, 6);
         delineation.onData(bufferWith(new byte[]{1, 2, 3}), 0, 3);
 
         assertEquals(
@@ -141,6 +142,22 @@ class IntegerLengthProviderFieldDelineationTest
     {
         final IntegerLengthFieldDelineation delineation = new IntegerLengthFieldDelineation(delineatedDataSpy, 0);
         delineation.onData(bufferWith(b(intInBytes(3)[0])), 0, 1);
+        delineation.onData(bufferWith(b(intInBytes(3)[1], intInBytes(3)[2])), 0, 2);
+        delineation.onData(bufferWith(b(intInBytes(3)[3])), 0, 1);
+        delineation.onData(bufferWith(new byte[]{1, 2, 3}), 0, 3);
+
+        assertEquals(
+                delineatedDataSpy.received(),
+                new byte[]{1, 2, 3}
+        );
+    }
+
+    @Test
+    @Disabled
+    void shouldUseLengthBrokenDownIntoMultipleBatchesWithPadding()
+    {
+        final IntegerLengthFieldDelineation delineation = new IntegerLengthFieldDelineation(delineatedDataSpy, 1);
+        delineation.onData(bufferWith(b(PADDING, intInBytes(3)[0])), 0, 1);
         delineation.onData(bufferWith(b(intInBytes(3)[1], intInBytes(3)[2])), 0, 2);
         delineation.onData(bufferWith(b(intInBytes(3)[3])), 0, 1);
         delineation.onData(bufferWith(new byte[]{1, 2, 3}), 0, 3);
