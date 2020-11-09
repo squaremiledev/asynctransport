2d7b55ed1e02a490e2cb2c341d253f3ab64658e5
Michael Szymczak <contact@michaelszymczak.com>
2020-11-09 21:56:30 +0000

    #6 Name log appropriately

--- asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/PrintingMessageListener.java
+++ asynctcpacceptance/src/test/java/dev/squaremile/asynctcpacceptance/PrintingMessageListener.java
@@ -2,11 +2,6 @@ package dev.squaremile.asynctcpacceptance;
 
 import org.agrona.DirectBuffer;
 import org.agrona.ExpandableRingBuffer;
-import org.agrona.concurrent.UnsafeBuffer;
-import org.agrona.concurrent.ringbuffer.OneToOneRingBuffer;
-import org.agrona.concurrent.ringbuffer.RingBuffer;
-
-import static org.agrona.concurrent.ringbuffer.RingBufferDescriptor.TRAILER_LENGTH;
 
 
 import dev.squaremile.asynctcp.sbe.MessageHeaderDecoder;
@@ -22,13 +17,12 @@ public class PrintingMessageListener implements SerializedMessageListener
     private final TransportEventDecoders transportEventDecoders = new TransportEventDecoders();
     private final TransportCommandDecoders transportCommandDecoders = new TransportCommandDecoders(new CommandsProvidingTransport(1234, rawStreaming()));
     private final MessageHeaderDecoder headerDecoder = new MessageHeaderDecoder();
-    private final RingBuffer logBuffer = new OneToOneRingBuffer(new UnsafeBuffer(new byte[1024 * 1024 + TRAILER_LENGTH]));
-    private final ExpandableRingBuffer expandableRingBuffer = new ExpandableRingBuffer();
-    private final StringBuilder log = new StringBuilder();
+    private final ExpandableRingBuffer applicationMessagesLog = new ExpandableRingBuffer();
+    private final StringBuilder humanReadableLog = new StringBuilder();
     private final SerializedMessageListener messageListener = (buffer, sourceOffset, length) ->
     {
-        log.append(decode(buffer, sourceOffset, length));
-        log.append("\n");
+        humanReadableLog.append(decode(buffer, sourceOffset, length));
+        humanReadableLog.append("\n");
     };
 
     private Object decode(final DirectBuffer sourceBuffer, final int sourceOffset, final int length)
@@ -47,14 +41,13 @@ public class PrintingMessageListener implements SerializedMessageListener
     @Override
     public void onSerialized(final DirectBuffer sourceBuffer, final int sourceOffset, final int length)
     {
-        logBuffer.write(1, sourceBuffer, sourceOffset, length);
-        expandableRingBuffer.append(sourceBuffer, sourceOffset, length);
+        applicationMessagesLog.append(sourceBuffer, sourceOffset, length);
     }
 
     public String logContent()
     {
-        log.setLength(0);
-        expandableRingBuffer.forEach(
+        humanReadableLog.setLength(0);
+        applicationMessagesLog.forEach(
                 (buffer, offset, length, headOffset) ->
                 {
                     messageListener.onSerialized(buffer, offset, length);
@@ -62,8 +55,8 @@ public class PrintingMessageListener implements SerializedMessageListener
                 },
                 Integer.MAX_VALUE
         );
-        String content = log.toString();
-        log.setLength(0);
+        String content = humanReadableLog.toString();
+        humanReadableLog.setLength(0);
         return content;
     }
 }
